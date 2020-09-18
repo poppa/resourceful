e048ce0740b2e897a73fb949e6e5a6ef6f68a068
Pontus Östlund <ponost@roxen.com>
2020-09-18 08:27:44 +0200

    `withChildren()` not need since we have an exception for proptypes in eslint by now

--- src/ui/components/PageState/PageState.tsx
+++ src/ui/components/PageState/PageState.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react'
 import { observer } from 'mobx-react'
 import { pageStateStore, projectsStore } from '../../storage'
-import { withChildren } from '../../lib'
 import { PageState } from '../../storage/pagestate'
 import HomeScreen from '../Homescreen'
 import Projects from '../Projects/Projects'
@@ -10,35 +9,33 @@ import EditProjectDialog from '../Projects/EditProjectDialog'
 import EditResourceDialog from '../Resource/EditResourceDialog'
 
 const PageStateComp: FC = observer(
-  withChildren(
-    (): JSX.Element => {
-      switch (pageStateStore.state) {
-        case PageState.Initializing:
-          return <div>Initializing...</div>
+  (): JSX.Element => {
+    switch (pageStateStore.state) {
+      case PageState.Initializing:
+        return <div>Initializing...</div>
 
-        case PageState.HomeScreen:
-          return (
-            <>
-              <CreateProjectDialog />
-              <HomeScreen />
-            </>
-          )
+      case PageState.HomeScreen:
+        return (
+          <>
+            <CreateProjectDialog />
+            <HomeScreen />
+          </>
+        )
 
-        case PageState.Projects:
-          return (
-            <>
-              <CreateProjectDialog />
-              <EditProjectDialog />
-              <EditResourceDialog />
-              <Projects projects={projectsStore.projects} />
-            </>
-          )
+      case PageState.Projects:
+        return (
+          <>
+            <CreateProjectDialog />
+            <EditProjectDialog />
+            <EditResourceDialog />
+            <Projects projects={projectsStore.projects} />
+          </>
+        )
 
-        default:
-          return <div>Unknown page state</div>
-      }
+      default:
+        return <div>Unknown page state</div>
     }
-  )
+  }
 )
 
 export default PageStateComp
